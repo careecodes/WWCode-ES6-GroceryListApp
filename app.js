2ec901646ace32bba15901a7284fca00d9697cf1
Caree Claunch <caree@caree.codes>
2018-10-11 11:09:36 -0500

    blank input validation

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const groceryList = document.querySelector('#grocery_list');
 const addToListButton = document.querySelector('#add_to_list');
 const groceryListInput = document.querySelector("#grocery_list_input");
+
 let addToList = () => {
-	groceryList.innerHTML += `<li>${groceryListInput.value}</li>`;
-	groceryListInput.value = '';
+    if (groceryListInput.value !== '') {
+        groceryList.innerHTML += `<li>${groceryListInput.value}</li>`;
+        groceryListInput.value = '';
+    }
 }
 
 addToListButton.addEventListener('click', ()=>{
 	addToList();
 })
-
-// addToList.addEventListener('submit', ()=>{
-// 	groceryList.innerHTML += `<li> ${groceryListInput.value} </li>`;
-// })
 
 // Hacking it without a form/submit event
 // Is this the right way? hmm...
